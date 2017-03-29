17fb232d41868bf6406d412d64231a66845bc0fc
matttse <tsemd@bu.edu>
2017-03-29 15:38:00 -0400

    todo parsing and storing

--- src/AllFriends.java
+++ src/AllFriends.java
@@ -2,8 +2,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.TreeMap;
 
 import dhl.UserInputHandler;
+import dhl.regexInput;
 
 /**
  * @author Matthew Tse
@@ -38,9 +41,13 @@ public class AllFriends {
 			String name = "";
 			int size = 0;
 			int cnt = 0;
+			// instantiate the the customer list object	
+			TreeMap<String, ArrayList> friendList = new TreeMap<String, ArrayList>();
+			ArrayList<Integer> friendValues = new ArrayList<Integer>();
 			String[] namesArray = null;
-			String[] fileData;
-			String data = "";
+			String fileData = null;
+			String[] data = null;
+			int idxStartCount = 0;
 			//while there is an expression to read
 			while ((line = bufferedReader.readLine()) != null) {
 				if (cnt == 0) {//first line is number of people in file
@@ -48,9 +55,11 @@ public class AllFriends {
 				} else if (cnt == 1) {//store names
 					namesArray = new String[size];
 					namesArray = line.split("\\s");
-				} else {					
+				} else {	
+//					line = line.replaceAll(" ", "");
+					data = line.split("\\s+");
 					stringBuffer.append(line);
-					stringBuffer.append("\n");
+//					data = fileData[cnt].trim().split(" ");
 				}
 				
 				
@@ -59,22 +68,13 @@ public class AllFriends {
 			//cleanup
 			stringBuffer.trimToSize();
 			fileReader.close();
-			fileData = stringBuffer.toString().split("\\s+");
+			
 			System.out.println("Contents of file:");
-//			System.out.println(size);
-			for (int i = 0; i < fileData.length; i++) {
-				//^\\s+|\\s+$|(\\s)+
-				/*
-				System.out.format("[%s]%n",
-				    test.replaceAll("^ +| +$|( )+", "$1")
-				*/
-				data += fileData[i].trim().replaceAll(" ", "").replaceAll("\\s", "");
-				System.out.println(fileData[i]);	
-				
-			}
+			System.out.println(fileData);
 			for (int i = 0; i < size; i++) {
 				if (i == 0) {//remove tabbed or spaces from first name
-					System.out.println(namesArray[i].trim().replaceAll(" ", "").replaceAll("\\s", ""));	
+					System.out.println(namesArray[i].trim().replaceAll(" ", "").replaceAll("\\s", ""));
+//					friendList.put(namesArray[i].trim().replaceAll(" ", "").replaceAll("\\s", ""), arg1);	
 				} else {
 					System.out.println(namesArray[i]);
 				}
@@ -84,22 +84,18 @@ public class AllFriends {
 			
 			// instantiate the handler
 			UserInputHandler<String> processInput = new UserInputHandler<String>();
+			// instantiate regex runner object
+			regexInput myVal = new regexInput();
 			// Option selector
 			while (completeFlag == 0) {
 				name = processInput.getAlphaNum("Enter then name of a person whose friends you want to find (enter 0 for exit): ");
-				try {
-					if (Integer.parseInt(name) != 0) {
-						System.out.println("Not a valid number");
-					} else if (Integer.parseInt(name) == 0) {
-						completeFlag = 0;
-						System.out.println("Thank you and goodbye");
-						exit(0);
-					}
-				} catch (Exception e) {
-					System.out.println("Input Error caused by NFE: " + e);
-				}
 
-				
+
+				if (name.equals("0")) {
+					completeFlag = 1;
+					System.out.println("Thank you and goodbye");
+					exit(0);
+				} 				
 			}
 
 
