3a4a701636ce3054dce5d649e7d6a0824d6f76dc
matttse <tsemd@bu.edu>
2017-03-30 11:41:03 -0400

    todo getfriendslist recusrively

--- src/AllFriends.java
+++ src/AllFriends.java
@@ -4,7 +4,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.Map.Entry;
 
 import dhl.UserInputHandler;
 import dhl.regexInput;
@@ -49,47 +53,55 @@ public class AllFriends {
 			String[] namesArray = null;
 			String[] row = null;
 			int[] intData = null;
+			AllFriends output = new AllFriends();
 			//while there is an expression to read
 			while ((line = bufferedReader.readLine()) != null) {
 				if (cnt == 0) {//first line is number of people in file
 					size = Integer.parseInt(line);
 				} else if (cnt == 1) {//store names
 					namesArray = new String[size];
-					namesArray = line.split("\\s");
+					namesArray = line.replaceAll("\\s+", " ").split(" ");
 				} else {	
-//					line = line.replaceAll(" ", "");
 					row = line.split("\\s+");
-					intData = new int[row.length];
-					for (int i = 1; i < row.length; i++) {
-						if (i == 1) {
-							intData[0] = Integer.parseInt(row[i+1]);	
-						} else {
-							intData[i] = Integer.parseInt(row[i]);
-						}
-						
+					intData = new int[(row.length-1)];//set array length offset name
+					for (int i = 0; i < (row.length-1); i++) {
+						intData[i] = Integer.parseInt(row[i+1]);
 					}
-					friendValues.add(intData);
-					friendList.put(namesArray[cnt], friendValues);
+					friendValues.add(intData);					
 				}
-				
-				
 				cnt++;
 			}//end while expressions
 			fileReader.close();
 			
 			System.out.println("Contents of file:");
 			
-			for (int i = 0; i < size; i++) {
+			for (int i = 0; i < namesArray.length; i++) {
 				if (i == 0) {//remove tabbed or spaces from first name
 					System.out.println(namesArray[i].trim().replaceAll(" ", "").replaceAll("\\s", ""));
 //					friendList.put(namesArray[i].trim().replaceAll(" ", "").replaceAll("\\s", ""), arg1);	
 				} else {
 					System.out.println(namesArray[i]);
+
 				}				
 			}
-			for (int i = 0; i < size; i++) {
-				System.out.println(friendList.get(namesArray[i]).iterator());
+			for (int j = 0; j < friendValues.size(); j++) {
+				int[] temp = friendValues.get(j);
+				for (int i = 0; i < temp.length; i++) {
+					System.out.print(temp[i]);	
+				}
+				System.out.print("\n");
+				
 			}
+//			Set<Entry<String, ArrayList>> set = friendList.entrySet();
+//			
+//			Iterator<Entry<String, ArrayList>> idx = set.iterator();
+//			
+//			while(idx.hasNext()) {
+//				Map.Entry<String, ArrayList> fList = (Map.Entry<String, ArrayList>)idx.next();
+//				
+//				System.out.println(fList.getValue());
+//			}
+			
 			
 			// instantiate the handler
 			UserInputHandler<String> processInput = new UserInputHandler<String>();
@@ -98,13 +110,15 @@ public class AllFriends {
 			// Option selector
 			while (completeFlag == 0) {
 				name = processInput.getAlphaNum("Enter then name of a person whose friends you want to find (enter 0 for exit): ");
-
-
-				if (name.equals("0")) {
+				if (output.getFriendList(name, friendValues, namesArray) != null) {
+					output.getFriendList(name, friendValues, namesArray);	
+				} else if (name.equals("0")) {
 					completeFlag = 1;
 					System.out.println("Thank you and goodbye");
 					exit(0);
-				} 				
+				} else {
+					System.out.println("Name Not Found, Try Again.");
+				}
 			}
 
 
@@ -112,6 +126,23 @@ public class AllFriends {
 			e.printStackTrace();
 		}//end trycatch
 	}
+	
+	
+	public String getFriendList(String name, ArrayList<int[]> friendValues, String[] namesArray) {
+		String friendList = null;
+		int idx = -1;
+		for (int i = 0; i < namesArray.length; i++) {
+			if (name.equals(namesArray[i])) {
+				idx = i;
+				break;
+			}
+		}
+		
+		
+		return friendList;
+	}
+	
+	
 	// standard system exit.
 	public static void exit(int status) {
 		System.exit(status);
