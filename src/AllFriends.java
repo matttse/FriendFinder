89de5cdd1e9eb7386f7b1ba5fe4fa7a73492aca0
Matt Tse <tsemd@bu.edu>
2017-04-04 18:57:35 -0400

    bug fix when first data int is 1

--- src/AllFriends.java
+++ src/AllFriends.java
@@ -148,10 +148,13 @@ public class AllFriends {
 		if (idx != -1) {//check name found
 			//get friend indexes (non zero)
 			for (int i = 0; i < namesArray.length; i++) {
-				if (!visited[i] && name.equals(namesArray[i])) {//check if name has been visited and name is correct
-					visited[i] = true;//set visit true
-					intData = friendValues.get(getPersonId(namesArray, name));//get friend data									
-					findFriends(intData, namesArray, friendValues, visited, i);
+				if (!visited[i] && !name.equals(namesArray[i])) {//check if name has been visited and name is correct
+					visited[i] = true;//set visit true					
+					intData = friendValues.get(getPersonId(namesArray, name));//get friend data		
+					if (intData[i] == 1) {
+						System.out.println(namesArray[i]);
+					}
+					findFriends(intData, namesArray, friendValues, visited, i, name);
 				}
 			}			
 					
@@ -179,13 +182,13 @@ public class AllFriends {
 	 * 
 	 * @Return null
 	 */	
-	public static void findFriends(int[] friendData, String[] namesArray, ArrayList<int[]> friendValues, boolean[] visited, int id) {
+	public static void findFriends(int[] friendData, String[] namesArray, ArrayList<int[]> friendValues, boolean[] visited, int id, String name) {
 		for (int i = 0; i < friendData.length; i++) {//loop through each row of whether a friend is marked
-			if (!visited[i] && i != id && friendData[i] != 0) {//check if already been visited, is ownself, and is a friend
+			if (!visited[i] && friendData[i] != 0 && !name.equals(namesArray[i])) {//check if already been visited, is ownself, and is a friend
 				visited[i] = true;
 				System.out.println(namesArray[i]);
 				friendData = friendValues.get(i);
-				findFriends(friendData, namesArray, friendValues, visited, i);//recursive
+				findFriends(friendData, namesArray, friendValues, visited, id, name);//recursive
 			}
 		}
 	}//end findFriends method
